4bc5037eb95bf1c75afbb23fe1e9f3d39fc8a537
smthl1keyou <145595637+smthl1keyou@users.noreply.github.com>
2024-01-02 20:58:11 +0700

    Update userprofile.dart

--- lib/pages/userprofile.dart
+++ lib/pages/userprofile.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pawfectly/constants/constants.dart';
+import 'package:pawfectly/pages/userprofile_update.dart';
 import 'package:pawfectly/widgets/info_container.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserProfile extends StatefulWidget {
   const UserProfile({super.key});
@@ -11,83 +14,140 @@ class UserProfile extends StatefulWidget {
 class _UserProfileState extends State<UserProfile> {
   @override
   Widget build(BuildContext context) {
+    var deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Color(0xffFFF2DE),
+      backgroundColor: const Color(0xffFFF2DE),
       appBar: AppBar(
+        elevation: 0,
         leading: InkWell(
           onTap: () {
             Navigator.pop(context);
           },
-          child: Icon(Icons.arrow_back, color: Color(0xff704520)),
+          child: const Icon(Icons.arrow_back, color: Color(0xff704520)),
         ),
-        title: Text(
+        title: const Text(
           "My Profile",
-          style: TextStyle(color: Color(0xff704520), fontWeight: FontWeight.w600),
+          style:
+              TextStyle(color: Color(0xff704520), fontWeight: FontWeight.w600),
         ),
-        backgroundColor: Color(0xffFFF2DE),
+        backgroundColor: const Color(0xffFFF2DE),
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center, // Center content vertically
-              children: [
-                Stack(
-                  children: [
-                    ClipOval(
-                      child: Image.asset(
-                        'assets/girl.jpg',
-                        width: 120,
+      body: FutureBuilder(
+          future: SharedPreferences.getInstance(),
+          builder: (context, snapshot) {
+            return Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment:
+                        MainAxisAlignment.center, // Center content vertically
+                    children: [
+                      Stack(
+                        children: [
+                          snapshot.data!.getString('image')! == 'null'
+                              ? Container(
+                                  decoration: const BoxDecoration(
+                                    color: Colors.grey,
+                                    shape: BoxShape.circle,
+                                  ),
+                                  width: MediaQuery.of(context).size.width / 4,
+                                  height: MediaQuery.of(context).size.width / 4,
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Icon(
+                                        Icons.person,
+                                        color: Colors.white,
+                                        size:
+                                            MediaQuery.of(context).size.width /
+                                                10,
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              : ClipOval(
+                                  child: Container(
+                                    width: deviceWidth / 4,
+                                    height: deviceWidth / 4,
+                                    decoration: const BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Colors.white,
+                                    ),
+                                    child: Image.network(
+                                      '$urlIp/storage/${snapshot.data!.getString('image')!}',
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                          // const Positioned(
+                          //   bottom: 1,
+                          //   right: 1,
+                          //   child: Icon(
+                          //     Icons.add_a_photo,
+                          //     size: 40,
+                          //     color: Color(0xffCC5946),
+                          //   ),
+                          // )
+                        ],
                       ),
-                    ),
-                    Positioned(
-                      bottom: 1,
-                      right: 1,
-                      child: Icon(
-                        Icons.add_a_photo,
-                        size: 40,
-                        color: Color(0xffCC5946),
-                      ),
-                    )
-                  ],
-                ),
-
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 60),
-                  child: Center(
-                    child: Column(
-                      children: [
-                        buildInfoContainer("assets/email.svg", "reginaaa@gmail.com"),
-                        buildInfoContainer("assets/username.svg", "Regina"),
-                        buildInfoContainer("assets/id-card.svg", "Regina George"),
-                        buildInfoContainer("assets/telephone-call.svg", "08123854768"),
-                        SizedBox(height: 60),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            primary: Color(0xffCC5946),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8.0, vertical: 60),
+                        child: Center(
+                          child: Column(
+                            children: [
+                              buildInfoContainer("assets/email.svg",
+                                  snapshot.data!.getString('email')!),
+                              buildInfoContainer("assets/username.svg",
+                                  snapshot.data!.getString('username')!),
+                              buildInfoContainer("assets/id-card.svg",
+                                  snapshot.data!.getString('name')!),
+                              buildInfoContainer("assets/telephone-call.svg",
+                                  snapshot.data!.getString('phone')!),
+                              const SizedBox(height: 60),
+                              ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: const Color(0xffCC5946),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.push(context,
+                                        MaterialPageRoute(builder: (context) {
+                                      return UpdateUserProfile(
+                                        email:
+                                            snapshot.data!.getString('email')!,
+                                        name: snapshot.data!.getString('name')!,
+                                        username: snapshot.data!
+                                            .getString('username')!,
+                                        phone:
+                                            snapshot.data!.getString('phone')!,
+                                        imageUrl:
+                                            snapshot.data!.getString('image')!,
+                                      );
+                                    })).then((value) {
+                                      setState(() {});
+                                    });
+                                  },
+                                  child: const Padding(
+                                    padding: EdgeInsets.all(10.0),
+                                    child: Text(
+                                      "Update Data",
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w600,
+                                          color: Color(0xffFFF2DE)),
+                                    ),
+                                  ))
+                            ],
                           ),
-                          onPressed: (){
-                            Navigator.pop(context);
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Text("Pawfect!",
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                              color: Color(0xffFFF2DE)
-                            ),),
-                          ))
-                      ],
-                    ),
+                        ),
+                      )
+                    ],
                   ),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
+                ),
+              ),
+            );
+          }),
     );
   }
 }
