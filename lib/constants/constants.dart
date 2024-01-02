4911b98194e131fec5fd717a04ed1a85ca430d66
smthl1keyou <145595637+smthl1keyou@users.noreply.github.com>
2024-01-02 20:25:31 +0700

    Update constants.dart

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,1 +1,4 @@
-String url = 'http://192.168.68.104:8000/api/';
+String url = 'https://sukucadang.encepmotor.com/pawfectly/public/api/';
+String urlIp = 'https://sukucadang.encepmotor.com/pawfectly/public';
+// String url = 'http://192.168.1.14:6969/api/';
+// String urlIp = 'http://192.168.1.14:6969';
