3afad7ca9a77a07bbb24ce3b6c4702d30ebc46cf
smthl1keyou <145595637+smthl1keyou@users.noreply.github.com>
2024-01-02 20:57:49 +0700

    Update signup.dart

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -5,43 +5,52 @@ import 'package:pawfectly/controllers/register_controller.dart';
 import 'package:pawfectly/pages/signin.dart';
 
 class SignUpPage extends StatelessWidget {
-  SignUpPage({Key? key});
+  SignUpPage({super.key});
   final RegisterController _registerController = Get.put(RegisterController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Color(0xffCC5946),
+        color: const Color(0xffCC5946),
         child: Stack(
           fit: StackFit.expand,
           children: [
             SingleChildScrollView(
-              child: Positioned(
-                child: Form(
-                key: _registerController.formKey,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                child: Column(
-                  children: [
-                    SizedBox(height: 225),
+              child: Form(
+                  key: _registerController.formKey,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 225),
                       _buildTextField(
                         controller: _registerController.emailController,
                         hintText: "e-mail",
-                        prefixIcon: SvgPicture.asset("assets/email.svg", color: Color(0xffCC5946), width: 24,),
+                        prefixIcon: SvgPicture.asset(
+                          "assets/email.svg",
+                          color: const Color(0xffCC5946),
+                          width: 24,
+                        ),
                         keyboardType: TextInputType.emailAddress,
                         validator: _registerController.validateEmail,
                       ),
                       _buildTextField(
                         controller: _registerController.nameController,
                         hintText: "Full Name",
-                        prefixIcon: SvgPicture.asset("assets/id-card.svg", color: Color(0xffCC5946), width: 24,),
+                        prefixIcon: SvgPicture.asset(
+                          "assets/id-card.svg",
+                          color: const Color(0xffCC5946),
+                          width: 24,
+                        ),
                       ),
                       _buildTextField(
                         controller: _registerController.usernameController,
                         hintText: "Username",
                         prefixIcon: SvgPicture.asset("assets/username.svg"),
                         validator: (value) {
-                          if (value == null || value.isEmpty || value.length < 6) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              value.length < 6) {
                             return 'Username must be at least 6 characters';
                           }
                           return null;
@@ -50,7 +59,11 @@ class SignUpPage extends StatelessWidget {
                       _buildTextField(
                         controller: _registerController.phoneController,
                         hintText: "Phone Number",
-                        prefixIcon: SvgPicture.asset("assets/telephone-call.svg", color: Color(0xffCC5946), height: 24,),
+                        prefixIcon: SvgPicture.asset(
+                          "assets/telephone-call.svg",
+                          color: const Color(0xffCC5946),
+                          height: 24,
+                        ),
                         keyboardType: TextInputType.phone,
                         validator: _registerController.validatePhone,
                       ),
@@ -58,9 +71,14 @@ class SignUpPage extends StatelessWidget {
                         controller: _registerController.passwordController,
                         context: context,
                         hintText: "Password",
-                        prefixIcon: SvgPicture.asset("assets/padlock.svg",color: Color(0xffCC5946), height: 30,),
+                        prefixIcon: SvgPicture.asset(
+                          "assets/padlock.svg",
+                          color: const Color(0xffCC5946),
+                          height: 30,
+                        ),
                         validator: (value) {
-                          final hasLetter = RegExp(r'[a-zA-Z]').hasMatch(value ?? '');
+                          final hasLetter =
+                              RegExp(r'[a-zA-Z]').hasMatch(value ?? '');
                           final hasNumber = RegExp(r'\d').hasMatch(value ?? '');
 
                           if (value == null ||
@@ -72,14 +90,18 @@ class SignUpPage extends StatelessWidget {
                           return null;
                         },
                       ),
-
                       _PasswordField(
                         controller: _registerController.conpasswordController,
                         context: context,
                         hintText: "Confirm Password",
-                        prefixIcon: SvgPicture.asset("assets/padlock.svg",color: Color(0xffCC5946), height: 30,),
+                        prefixIcon: SvgPicture.asset(
+                          "assets/padlock.svg",
+                          color: const Color(0xffCC5946),
+                          height: 30,
+                        ),
                         validator: (value) {
-                          final hasLetter = RegExp(r'[a-zA-Z]').hasMatch(value ?? '');
+                          final hasLetter =
+                              RegExp(r'[a-zA-Z]').hasMatch(value ?? '');
                           final hasNumber = RegExp(r'\d').hasMatch(value ?? '');
 
                           if (value == null ||
@@ -91,21 +113,16 @@ class SignUpPage extends StatelessWidget {
                           return null;
                         },
                       ),
-
-                      
-                      SizedBox(height: 12),
+                      const SizedBox(height: 12),
                       _buildElevatedButton(
                         onPressed: () {
                           _registerController.register(context);
                         },
                         label: "Get Started",
                       ),
-                  ],
-                )
-              ),
-              ),
+                    ],
+                  )),
             ),
-
             Positioned(
               top: 0,
               left: 0,
@@ -120,45 +137,43 @@ class SignUpPage extends StatelessWidget {
               left: 0,
               right: 0,
               child: Column(
-                  children: [
-                    Center(
-                      child: Image(
-                        image: AssetImage("assets/logo.png"),
-                      ),
+                children: [
+                  const Center(
+                    child: Image(
+                      image: AssetImage("assets/logo.png"),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 75.0, vertical: 25),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          InkWell(
-                            onTap: () {
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => SignInPage(),
-                                  fullscreenDialog: true,
-                                ),
-                              );
-                            },
-                            child: Text(
-                              "SignIn",
-                              style: _textStyle(),
-                            ),
-                          ),
-                          Text(
-                            "SignUp",
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 75.0, vertical: 25),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SignInPage(),
+                                fullscreenDialog: true,
+                              ),
+                            );
+                          },
+                          child: Text(
+                            "SignIn",
                             style: _textStyle(),
                           ),
-                        ],
-                      ),
+                        ),
+                        Text(
+                          "SignUp",
+                          style: _textStyle(),
+                        ),
+                      ],
                     ),
-                    
-                  ],
-                ),
+                  ),
+                ],
+              ),
             ),
-            
           ],
         ),
       ),
@@ -181,13 +196,13 @@ class SignUpPage extends StatelessWidget {
         controller: controller,
         validator: validator,
         decoration: InputDecoration(
-          fillColor: Color(0xffFCF2F1),
+          fillColor: const Color(0xffFCF2F1),
           filled: true,
           hintText: hintText,
-          hintStyle: TextStyle(
+          hintStyle: const TextStyle(
             color: Color.fromARGB(255, 222, 136, 128),
           ),
-          contentPadding: EdgeInsets.all(12.0),
+          contentPadding: const EdgeInsets.all(12.0),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(25.0),
             borderSide: BorderSide.none,
@@ -196,9 +211,9 @@ class SignUpPage extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 24),
             child: prefixIcon,
           ),
-          errorStyle: TextStyle(color: Colors.white),
+          errorStyle: const TextStyle(color: Colors.white),
         ),
-        style: TextStyle(),
+        style: const TextStyle(),
       ),
     );
   }
@@ -208,33 +223,32 @@ class SignUpPage extends StatelessWidget {
     required String label,
   }) {
     return ElevatedButton(
-    
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color.fromARGB(255, 252, 196, 118),
+        minimumSize: const Size(111, 46),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      onPressed: onPressed,
       child: Text(
         label,
-        style: TextStyle(
+        style: const TextStyle(
           color: Color(0xFF704520),
           fontSize: 16,
         ),
       ),
-      style: ElevatedButton.styleFrom(
-        primary: Color.fromARGB(255, 252, 196, 118),
-        minimumSize: Size(111, 46),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-      ), onPressed: onPressed,
     );
   }
 
   TextStyle _textStyle({Color? color}) {
     return TextStyle(
-      color: color ?? Color(0xffCC5946),
+      color: color ?? const Color(0xffCC5946),
       fontSize: 18,
       fontWeight: FontWeight.normal,
     );
   }
 }
-
 
 class _PasswordField extends StatefulWidget {
   const _PasswordField({
@@ -267,13 +281,13 @@ class _PasswordFieldState extends State<_PasswordField> {
         controller: widget.controller,
         obscureText: !isPasswordVisible,
         decoration: InputDecoration(
-          fillColor: Color(0xffFCF2F1),
+          fillColor: const Color(0xffFCF2F1),
           filled: true,
           hintText: widget.hintText,
-          hintStyle: TextStyle(
+          hintStyle: const TextStyle(
             color: Color.fromARGB(255, 222, 136, 128),
           ),
-          contentPadding: EdgeInsets.all(12.0),
+          contentPadding: const EdgeInsets.all(12.0),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(25.0),
             borderSide: BorderSide.none,
@@ -289,15 +303,13 @@ class _PasswordFieldState extends State<_PasswordField> {
               });
             },
             child: Icon(
-              isPasswordVisible
-                  ? Icons.visibility
-                  : Icons.visibility_off,
-              color: Color(0xffCC5946),
+              isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+              color: const Color(0xffCC5946),
             ),
           ),
-          errorStyle: TextStyle(color: Colors.white),
+          errorStyle: const TextStyle(color: Colors.white),
         ),
-        style: TextStyle(fontSize: 18),
+        style: const TextStyle(fontSize: 18),
         autovalidateMode: AutovalidateMode.onUserInteraction,
         validator: widget.validator,
       ),
