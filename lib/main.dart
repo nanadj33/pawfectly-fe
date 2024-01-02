ca201051fc81993b7a157146d12f26d8dfd67085
smthl1keyou <145595637+smthl1keyou@users.noreply.github.com>
2024-01-02 21:03:21 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:pawfectly/pages/pet_report.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:pawfectly/pages/homePage.dart';
 import 'package:pawfectly/pages/onboarding.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
+  initializeDateFormatting("id");
   runApp(const MyApp());
 }
 
@@ -17,10 +20,31 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Pawfectly',
       theme: ThemeData(
+        useMaterial3: false,
         textTheme: GoogleFonts.mPlusRounded1cTextTheme(
           Theme.of(context).textTheme,
         ),
       ),
-      home: OnboardingPage());
+      home: Scaffold(
+        body: FutureBuilder(
+            future: SharedPreferences.getInstance(),
+            builder: (context, prefs) {
+              var dataPrefs = prefs.data;
+              if (prefs.hasData) {
+                if (dataPrefs!.getString('id') != null) {
+                  return const HomePage();
+                } else {
+                  return const OnboardingPage();
+                }
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ); // Login Page
+              }
+            }),
+      ),
+    );
   }
 }
